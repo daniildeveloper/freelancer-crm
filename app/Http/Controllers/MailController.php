987d7681ba6732c3ab0e99693d38a91795568966
Daniil Borowkow <daniilborowkow@yandex.ru>
2017-01-31 07:46:40 +0000

    Now crm can send emails. Tested

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 class MailController extends Controller
 {
+
   /**
    * This function return view with index of mail client
    * @return view
@@ -11,5 +12,48 @@ class MailController extends Controller
   public function showIndex()
   {
     return view('mail.index');
+  }
+
+  public function writeEmail($subject, $from_email, $from_name, $addresses, $body, $alt_body = '', $attach = '')
+  {
+    //mail ru creds: maile: crm.clone@mail.ru pwd: secretsecret
+    $transport = \Swift_SmtpTransport::newInstance('smtp.mail.ru', 465, 'ssl')
+      ->setUsername('crm.clone@mail.ru')
+      ->setPassword('secretsecret');
+      
+    $mailer = \Swift_Mailer::newInstance($transport);
+
+    // $mailer = new \Swift_Mailer();
+    $message = \Swift_Message::newInstance();
+
+    // charset to utf8
+    $message->setCharset('iso-8859-2');
+
+    //give the msessage subject
+    $message->setSubject($subject);
+
+    //set from address with an assoc array
+    $message->setFrom([$from_email => $from_name]);
+
+    // set addressat
+    $message->setTo($addresses);
+
+    //give it a body
+    $message->setBody($body);
+
+    if ($alt_body !== '') {
+      // alternative text
+      $message->addPart($alt_body);
+    }
+
+    if ($attach !== '') {
+      // file attachments
+      $message->attach(\Swift_Attachment::fromPath('attachment.pdf'));
+    }
+
+    // send the message
+    $result = $mailer->send($message);
+
+    return $result;
   }
 }
---
+++ tests/Feature/MailControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Http\Controllers\MailController;
+use Tests\TestCase;
+
+class MailControllerTest extends TestCase
+{
+  /**
+   * A basic test example.
+   *
+   * @return void
+   */
+  public function testExample()
+  {
+    $this->assertTrue(true);
+  }
+
+  public function testWriteEmail()
+  {
+    $mc = new MailController();
+    $this->assertTrue($mc->writeEmail(
+      'First email from localhost',
+      'crm.clone@mail.ru',
+      'CRM clone',
+      ['daniilborowkow@yandex.ru'],
+      'This email is generated automaticly.'
+    ));
+
+  }
+}
